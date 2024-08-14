e439b4ca43ce3c77964ea6eb40e2c2814766b67a
renoker <rodolfoulises.ramirez@gmail.com>
2024-08-14 20:39:30 +0000

    Cambios servidor

---
+++ public/build/assets/cotizador-f75e5dea.js
@@ -0,0 +1,1 @@
+import{G as e}from"./glide.esm-4bd3ed45.js";new e("#relacionadas",{type:"carousel",startAt:0,perView:1,peek:{before:190,after:120},breakpoints:{600:{perView:1,peek:{before:50,after:50}}}}).mount();
